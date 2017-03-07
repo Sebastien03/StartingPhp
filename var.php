35c384542450f324e5db11658146624ababa7093
Sebastien Dutripon <seb032@live.fr>
2017-03-07 14:38:26 +0100

    Correction des bug Exercice 6

--- var.php
+++ var.php
@@ -1,36 +1,46 @@
-<?php
-// Exercice 1 //
-$nom= "Dutripon";
-// Exercice 2 //
-$prenom= "Sebastien";
-$age = 19;
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Php</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<?php
+		// Exercice 1 //
+		$nom= "Dutripon";
+		// Exercice 2 //
+		$prenom= "Sebastien";
+		$age = 19;
 
-echo $nom."<br/>";
-echo $prenom."<br/>";
-echo $age."<br/>";
-// Exercice 3 //
-$kilometre = 1;
-echo $kilometre."<br/>";
+		echo $nom."<br/>";
+		echo $prenom."<br/>";
+		echo $age."<br/>";
+		// Exercice 3 //
+		$kilometre = 1;
+		echo $kilometre."<br/>";
 
-$kilometre = 3;
-echo $kilometre."<br/>";
+		$kilometre = 3;
+		echo $kilometre."<br/>";
 
-$kilometre = 125;
-echo $kilometre."<br/>";
-// Exercice 4 //
-$string = "blabla";
-echo $string."<br/>";
-$int = 19;
-echo $int."<br/>";
-$float = 15.8;
-echo $float."<br/>";
-$bolean = true;
-echo $bolean."<br/>";
-// Exercice 5 //
-$int2;
-$int2 = 9; 
-echo $int2."<br/>";
-// Exercice 6 //
-$nom2 = "Dutripon";
-echo ("Bonjour,".$nom2.", ca va ?");
-?>
+		$kilometre = 125;
+		echo $kilometre."<br/>";
+		// Exercice 4 //
+		$string = "blabla";
+		echo $string."<br/>";
+		$int = 19;
+		echo $int."<br/>";
+		$float = 15.8;
+		echo $float."<br/>";
+		$bolean = true;
+		echo $bolean."<br/>";
+		// Exercice 5 //
+		$int2;
+		$int2 = 9; 
+		echo $int2."<br/>";
+		// Exercice 6 //
+		$nom2 = "Dutripon";
+		echo ("Bonjour,".$nom2.", ca va ?");
+		?>
+	</body>
+</html>
