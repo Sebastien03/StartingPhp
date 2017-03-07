89a896a1ea10bc9bfe62e85b99654e43b8482fd8
Sebastien Dutripon <seb032@live.fr>
2017-03-07 17:02:51 +0100

    Exercice 4

--- condition.php
+++ condition.php
@@ -18,22 +18,53 @@ $age2 = 19 ;
 $genre = "Homme";
 	if ($age2 >= 18) {
 		if ($genre == "Homme") {
-			echo "Vous etes un Homme majeure";
+			echo "Vous etes un Homme majeure"."<br/>";
 		} 
 	}
 	if ($age2 < 18) {
 		if ($genre == "Homme") {
-			echo "Vous etes un Homme mineure";
+			echo "Vous etes un Homme mineure"."<br/>";
 		}
 	}
 	if ($age2 >= 18) {
 		if ($genre == "Femme") {
-			echo "Vous etes une Femme majeure";
+			echo "Vous etes une Femme majeure"."<br/>";
 		}
 	}
 	if ($age2 < 18){
 		if ($genre == "Femme") {
-			echo "Vous etes une Femme mineure";
+			echo "Vous etes une Femme mineure"."<br/>";
 		}
+	}
+// Exercice 4 //
+$magnetude = 3 ;
+	switch ($magnetude) {
+		case 1:
+			echo "Micro-séisme impossible à ressentir."."<br/>";
+			break;
+		case 2:
+			echo "Micro-séisme impossible à ressentir mais enregistrable par les sismomètres."."<br/>";
+			break;
+		case 3:
+			echo "Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti."."<br/>"."<br/>";
+			break;
+		case 4:
+			echo " Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats."."<br/>";
+			break;
+		case 5:
+			echo " Séisme capable d'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes."."<br/>";
+			break;
+		case 6:
+			echo "Fort séisme capable d'engendrer des destructions majeures sur une large distance (180 km) autour de l'épicentre"."<br/>";
+			break;
+		case 7:
+			echo "Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance."."<br/>";
+			break;
+		case 8:
+			echo "Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres."."<br/>";
+			break;
+		case 9:
+			echo " Séisme capable de tout détruire sur une très vaste zone."."<br/>";
+			break;
 	}
 ?>
