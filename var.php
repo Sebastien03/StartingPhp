3a2a97cc67ab7928b50feaf6f101b7896b3b453f
Sebastien Dutripon <seb032@live.fr>
2017-03-07 14:11:35 +0100

    Exercice 4

--- var.php
+++ var.php
@@ -2,10 +2,24 @@
 $nom= "Dutripon";
 $prenom= "Sebastien";
 $age = 19;
-$kilometre = 125;
+$kilometre = 1;
 
 echo $nom;
 echo $prenom;
 echo $age;
 echo $kilometre;
+
+$kilometre = 3;
+echo $kilometre;
+
+$kilometre = 125;
+echo $kilometre;
+$string = "blabla";
+echo $string;
+$int = 19;
+echo $int;
+$float = 15.8;
+echo $float;
+$bolean = true;
+echo $bolean;
 ?>
