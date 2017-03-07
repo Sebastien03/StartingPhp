d03f967ef04307a1b3c1f17c7b3bcb9bc813792e
Sebastien Dutripon <seb032@live.fr>
2017-03-07 16:35:37 +0100

    Exercice 2 page 2

--- condition.php
+++ condition.php
@@ -1,8 +1,16 @@
 <?php
+// Exercice 1 //
 $age = 19;
 	if ($age >= 18) {
-		echo "Vous est majeur";
+		echo "Vous est majeur"."<br/>";;
 	} else {
-		echo "Vous etes mineur";
+		echo "Vous etes mineur"."<br/>";;
+	}
+// Exercice 2 //
+$IsEasy = true;
+	if ($IsEasy == true) {
+		echo "C'est facile"."<br/>";
+	} else {
+		echo "C'est difficile"."<br/>";
 	}
 ?>
