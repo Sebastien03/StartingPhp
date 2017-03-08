2f4f690112dd76d85acf0b1c99ad86273191afd8
Sebastien Dutripon <seb032@live.fr>
2017-03-08 16:18:40 +0100

    Exercice 6

--- user.php
+++ user.php
@@ -16,4 +16,14 @@
 		 echo htmlspecialchars($_POST["prenom2"])."<br/>";
 	}
 
+// Exercice 6 //
+	if (isset($_GET["type"])){
+		echo htmlspecialchars($_GET["type"])."<br/>";
+	}
+	if (isset($_GET["nom3"])){
+		echo htmlspecialchars($_GET["nom3"])."<br/>";
+	} 
+	if (isset($_GET["prenom3"])) {
+		 echo htmlspecialchars($_GET["prenom3"])."<br/>";
+	}
 ?>
