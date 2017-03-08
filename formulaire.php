ec52b1c19c557cb2d736b597f6ba094fbc8c067f
Sebastien Dutripon <seb032@live.fr>
2017-03-08 15:44:13 +0100

    Exercice 2

--- formulaire.php
+++ formulaire.php
@@ -7,12 +7,14 @@
 		</head>
 		<body>
 		<!-- Exercice 1 -->
+		<h1> Method GET </h1>
 			<form action="user.php" method="get">
 				<p> <input type="text" name="nom" /> nom </p>
 				<p> <input type="text" name="prenom" />prenom </p>
 				<p> <input type="submit" name="Ok"> Envoyer </p>
 			</form>
 		<!-- Exercice 2 -->
+		<h1> Method Post </h1>
 			<form action="user.php" method="post">
 				<p> <input type="text" name="nom" /> nom </p>
 				<p> <input type="text" name="prenom" />prenom </p>
